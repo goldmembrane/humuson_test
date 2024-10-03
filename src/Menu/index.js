860053024bde2154364a08915162a1e81560ef29
= <=>
2024-10-03 22:33:43 +0900

    feat: Refactor side menu constructor
    - 사이드 바 메뉴 html 구조 변경

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,70 +1,158 @@
 import { useState } from "react";
 
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import SendIcon from "@mui/icons-material/Send";
+import PeopleIcon from "@mui/icons-material/People";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import DonutSmallIcon from "@mui/icons-material/DonutSmall";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import EqualizerIcon from "@mui/icons-material/Equalizer";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import ExpandCircleDownOutlinedIcon from "@mui/icons-material/ExpandCircleDownOutlined";
 
 import { menus, selectIcon } from "../util/util";
 
 const Menu = () => {
-  const [isBoardOpen, setIsBoardOpen] = useState(false);
-
-  const handleBoard = () => {
-    setIsBoardOpen(!isBoardOpen);
-  };
-
-  const [isSendOpen, setIsSendOpen] = useState(false);
-
-  const handleSend = () => {
-    setIsSendOpen(!isSendOpen);
-  };
-
   return (
     <>
-      <div className="w-2/12 flex-col">
-        {menus.map((menu) => (
-          <>
-            <div
-              className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]"
-              key={menu.id}
-              onClick={() => {
-                if (menu.id === 1) handleBoard();
-                else if (menu.id === 2) handleSend();
-              }}
-            >
-              <div className="flex items-center">
-                <div>{selectIcon(menu.icon)}</div>
+      <nav className="w-2/12 flex-col">
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <CalendarMonthIcon />
 
-                <div className="ml-[10px] font-bold text-xl"> {menu.title}</div>
-              </div>
+            <div className="ml-[10px] font-bold text-xl">게시판</div>
+          </div>
 
-              <ExpandCircleDownOutlinedIcon
-                style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
-              />
-            </div>
-            {menu.subtitle &&
-              isBoardOpen &&
-              menu.id === 1 &&
-              menu.subtitle.map((subtitle) => (
-                <div
-                  className="px-10 py-3 text-xl cursor-pointer"
-                  key={subtitle.subId}
-                >
-                  {subtitle.title}
-                </div>
-              ))}
-            {menu.subtitle &&
-              isSendOpen &&
-              menu.id === 2 &&
-              menu.subtitle.map((subtitle) => (
-                <div
-                  className="px-10 py-3 text-xl cursor-pointer"
-                  key={subtitle.subId}
-                >
-                  {subtitle.title}
-                </div>
-              ))}
-          </>
-        ))}
-      </div>
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="px-10 py-3 text-xl cursor-pointer">공지사항</div>
+        <div className="px-10 py-3 text-xl cursor-pointer">Q&A</div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <SendIcon />
+
+            <div className="ml-[10px] font-bold text-xl">메시지</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="px-10 py-3 text-xl cursor-pointer">발송하기</div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <PeopleIcon />
+
+            <div className="ml-[10px] font-bold text-xl">캠페인</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <AssignmentIcon />
+
+            <div className="ml-[10px] font-bold text-xl">템플릿</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <DonutSmallIcon />
+
+            <div className="ml-[10px] font-bold text-xl">발송 관리</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <DonutSmallIcon />
+
+            <div className="ml-[10px] font-bold text-xl">리포트</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <AssessmentIcon />
+
+            <div className="ml-[10px] font-bold text-xl">통계</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <EqualizerIcon />
+
+            <div className="ml-[10px] font-bold text-xl">정산</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <PeopleIcon />
+
+            <div className="ml-[10px] font-bold text-xl">회원</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <PeopleIcon />
+
+            <div className="ml-[10px] font-bold text-xl">PUSH 사용자</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+
+        <div className="flex justify-between px-[10px] py-3 cursor-pointer border-b-2 border-[#e3e3e3]">
+          <div className="flex items-center">
+            <BookmarkBorderIcon />
+
+            <div className="ml-[10px] font-bold text-xl">대시보드</div>
+          </div>
+
+          <ExpandCircleDownOutlinedIcon
+            style={{ width: "30px", height: "30px", color: "#e3e3e3" }}
+          />
+        </div>
+      </nav>
     </>
   );
 };
